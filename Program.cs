acf3b183e86e6936d2fea2d15da672c0ba2cbe87
Arfiandimas <Arfiandimas@arfiandimas1929@gmail.com>
2022-06-23 14:31:43 +0700

    ADDED: update

--- Program.cs
+++ Program.cs
@@ -7,6 +7,15 @@ using System.Net;
 
 class Program
 {
+    static int id;
+    static string data;
+    static bool is_print;
+    static bool is_scan;
+    static string created_at;
+    static string updated_at;
+
+    // static bool check_reader;
+
     static MySqlConnection con = new MySqlConnection("Database=integra;Server=localhost;user=barjono;password=password");
 
     // Connection UDP Send
@@ -25,29 +34,30 @@ class Program
 
     static void SendData()
     {
+        bool check_reader = false;
         con.Open();
-
         using (MySqlCommand command = new MySqlCommand("SELECT * FROM print_scan WHERE is_print = 0 ORDER BY created_at ASC LIMIT 1", con))
         {
             MySqlDataReader reader = command.ExecuteReader();
-            while (reader.Read())
-            {
-                int id = reader.GetInt32(0);
-                string data = reader.GetString(1);
-                bool is_print = reader.GetBoolean(2);
-                bool is_scan = reader.GetBoolean(3);
-                string created_at = reader.GetString(4);
-                string updated_at = reader.GetString(5);
-
-                Console.WriteLine(data);
+            check_reader = reader.Read();
+            if (check_reader){
+                id = reader.GetInt32(0);
+                data = reader.GetString(1);
+                is_print = reader.GetBoolean(2);
+                is_scan = reader.GetBoolean(3);
+                created_at = reader.GetString(4);
+                updated_at = reader.GetString(5);
+                con.Close();
+                UdpSend(id, is_print, data);
+            } else {
+                con.Close();
             }
         }
-        Console.WriteLine(printScans);
-        // con.Close();
-        // UdpSend(printScans[0].ToString());
+        Console.WriteLine("Selesai");
+        Environment.Exit(0);
     }
 
-    static void UdpSend(string message = "")
+    static void UdpSend(int id_data, bool is_print_result, string message = "")
     {
         Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram,ProtocolType.Udp);
         IPAddress serverAddr = IPAddress.Parse("192.168.1.23");
@@ -62,10 +72,11 @@ class Program
                 Console.WriteLine("Waiting for response");
                 byte[] bytes = listener.Receive(ref groupEP);
                 string result = Encoding.ASCII.GetString(bytes, 0, bytes.Length);
-                if(result != message){
+                if(result != "success"){
                     Console.WriteLine("gagal");
                 } else {
                     Console.WriteLine(result);
+                    UpdateData(id_data, 1);
                     SendData();
                 }
             }
@@ -73,6 +84,18 @@ class Program
             Console.WriteLine(e);
         } finally {
             listener.Close();
+        }
+    }
+
+    static void UpdateData(int id, int is_print)
+    {
+        con.Open();
+        using (MySqlCommand cmd = new MySqlCommand("UPDATE print_scan SET is_print = @is_print WHERE id = @id", con))
+        {
+            cmd.Parameters.Add(new MySqlParameter("id", id));
+            cmd.Parameters.Add(new MySqlParameter("is_print", is_print));
+            cmd.ExecuteNonQuery();
+            con.Close();
         }
     }
 }
